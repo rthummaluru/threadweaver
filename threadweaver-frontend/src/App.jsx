6a2563ac52871dbcb332d4804863711099602bcd
Ronit Thummaluru <120275888+rthummaluru@users.noreply.github.com>
2025-11-08 20:13:17 -0500

    feat: implement App component with new layout and functionality, including AppName, Chat, QueryBar, and Button components

--- threadweaver-frontend/src/App.jsx
+++ threadweaver-frontend/src/App.jsx
@@ -1,10 +1,45 @@
-import TestHeader from './components/Headers';
-function App() {
+import React from 'react';
+import AppName from './components/AppName';
+import Headings from './components/Headers';
+import QueryBar from './components/QueryBar';
+import Chat from './components/Chat';
+import Button from './components/Button';
+
+const App = () => {
+  
+  // Acknowledge the click button event
+  const handleQuery = () => {
+    alert('Query Button Clicked!')
+  }
+
+
   return (
-    <div>
-      <TestHeader />
-    </div>
+    <>
+      <AppName>
+        <h1>ThreadWeaver</h1>
+      </AppName>
+      
+      <div className="headings-container">
+        <Headings>
+          <p>ThreadWeaver is a tool that helps you work seamlessly across Slack, Notion, and WhatsApp Business.</p>
+        </Headings>
+      </div>
+
+      <div className="chat-container">
+        <Chat>
+          <div>
+            <p>what can i help you with?</p>
+          </div>
+        </Chat>
+      </div>
+
+      <div className="search-bar-container">
+        <QueryBar>
+          <textarea className="query-input" placeholder="Enter your query" />
+          <Button textContent='Send' handleClick={handleQuery} />
+        </QueryBar>
+      </div>
+    </>
   )
 }
-
 export default App;
