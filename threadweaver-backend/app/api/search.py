e50936db4969b4e87a569af032b49a2a3906c846
Ronit Thummaluru <120275888+rthummaluru@users.noreply.github.com>
2025-12-09 21:09:58 -0500

    refactor: integrate RAGService for search functionality, simplifying query handling and enhancing error management

--- threadweaver-backend/app/api/search.py
+++ threadweaver-backend/app/api/search.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, UploadFile, File
 
 from app.db.supabase_client import get_supabase_connection
 from app.schemas.requests import SearchResponse, SearchResult
+from app.services.rag_service import RAGService
 from config import config
 import logging
 
@@ -11,52 +12,16 @@ router = APIRouter(prefix="/api/v1", tags=["search"])
 supabase_client = get_supabase_connection()
 
 @router.post("/search")
-async def search(query: str, match_threshold: float = 0.5, top_k: int = 5) -> SearchResponse:
+def search(query: str) -> SearchResponse:
     """
     Search the database for documents
     """
     logger.info(f"Searching for documents: {query}")
-    
+    rag_service = RAGService()
     try:
-        # Embed the user query
-        user_query_embedding = embed_user_query(query)
-        logger.info(f"User query embedded successfully: {user_query_embedding}")
-
-        # Search the database for documents
-        search_results = search_chunks(user_query_embedding, match_threshold, top_k)
-        logger.info(f"Search results: {search_results}")
-
-        return SearchResponse(
-            results=[SearchResult(
-                chunk_text=result["original_text"],
-                chunk_index=result["chunk_index"],
-                similarity_score=0.0,
-                document_title=result["document_id"]
-            ) for result in search_results.data]
-        )
-    except Exception as e:
-        logger.error(f"Error searching for documents: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
-
-def embed_user_query(query: str) -> list[float]:
-    """
-    Embed the user query
-    """
-    return config.get_embedding_model().embed_query(query)
-
-def search_chunks(user_query_embedding: list[float], match_threshold: float = 0.5, top_k: int = 5) -> list[SearchResponse]:
-    """
-    Search the database for chunks
-    """
-    try:
-        # Search the database for chunks
-        search_results = supabase_client.rpc("match_documents", {
-            "query_embedding": user_query_embedding,
-            "match_threshold": match_threshold,
-            "match_count": top_k
-        }).execute()
+        search_results = rag_service.search(query=query)
         logger.info(f"Search results: {search_results}")
         return search_results
     except Exception as e:
-        logger.error(f"Error searching for chunks: {e}")
+        logger.error(f"Error searching for documents: {e}")
         raise HTTPException(status_code=500, detail=str(e))
---
+++ threadweaver-backend/app/services/rag_service.py
@@ -0,0 +1,73 @@
+import logging
+import time as t
+from config import config
+
+from app.db.supabase_client import get_supabase_connection
+
+from app.schemas.requests import SearchResponse, SearchResult
+
+logger = logging.getLogger(__name__)
+
+supabase_client = get_supabase_connection()
+
+class RAGService:
+    """
+    RAG service class
+    """
+    def __init__(self):
+        """
+        Initialize the RAG service
+        """
+        self.supabase_client = supabase_client
+
+    
+    def _embed_user_query(self, query: str) -> list[float]:
+        """
+        Embed the user query
+        """
+        return config.get_embedding_model().embed_query(query)
+
+    def _search_chunks(self, user_query_embedding: list[float], match_threshold: float = 0.5, top_k: int = 5):
+        """
+        Search the database for chunks
+        """
+        try:
+            # Search the database for chunks
+            search_results = self.supabase_client.rpc("match_documents", {
+                "query_embedding": user_query_embedding,
+                "match_threshold": match_threshold,
+                "match_count": top_k
+            }).execute()
+            logger.info(f"Search results: {search_results}")
+            return search_results
+        except Exception as e:
+            logger.error(f"Error searching for chunks: {e}")
+            raise Exception(f"Error searching for chunks: {e}")
+    
+
+    def search(self, query: str, match_threshold: float = 0.2, top_k: int = 5) -> SearchResponse:
+        """
+        Search the database for documents
+        """
+        logger.info(f"Searching for documents: {query}")
+        
+        try:
+            # Embed the user query
+            user_query_embedding = self._embed_user_query(query)
+            logger.info(f"User query embedded successfully: {len(user_query_embedding)}")
+
+            # Search the database for documents
+            search_results = self._search_chunks(user_query_embedding, match_threshold, top_k)
+            logger.info(f"Search results: {search_results}")
+
+            return SearchResponse(
+                results=[SearchResult(
+                    chunk_text=result["original_text"],
+                    chunk_index=result["chunk_index"],
+                    similarity_score=0.0,
+                    document_title=result["document_id"]
+                ) for result in search_results.data]
+            )
+        except Exception as e:
+            logger.error(f"Error searching for documents: {e}")
+            raise Exception(f"Error searching for documents: {e}")
