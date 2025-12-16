a09a693e67e4569faf3d35495401a14eacc440fc
Ronit Thummaluru <120275888+rthummaluru@users.noreply.github.com>
2025-12-15 19:31:07 -0500

    feat: enhance LoginPage with email and password input fields and login button functionality

--- threadweaver-frontend/src/pages/LoginPage.tsx
+++ threadweaver-frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import Button from '../components/Button';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const handleLoginWithEmail = () => {
+    console.log('Login with email');
+}
 
 const LoginPage = () => {
     return (
-        <div>
+        <>
+        <div className="login-container flex flex-col items-center justify-center h-screen">
             <h1>Login</h1>
+            <form className="login-form flex flex-col gap-2 w-half max-w-md">
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <Button textContent="Login" handleClick={handleLoginWithEmail} />
+            </form>
         </div>
+        </>
     )
 }
 
